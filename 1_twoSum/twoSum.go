657fc8f263f5b53898e87f398eff34a7456309e6
xuzh <xzh7868@qq.com>
2019-10-27 14:22:08 +0800

    原数组不是有序的，以golang的方式进行排序，但内存消耗过大

--- 1_twoSum/twoSum.go
+++ 1_twoSum/twoSum.go
@@ -1,16 +1,35 @@
 package twoSum
 
+import "sort"
+
+type twoNum struct {
+	num int
+	index int
+}
+
+type twoNums []twoNum
+
+func (t twoNums) Len() int {return len(t)}
+func (t twoNums) Less(i, j int) bool {return t[i].num < t[j].num}
+func (t twoNums) Swap(i, j int) {t[i], t[j] = t[j], t[i]}
+
 func TwoSum(nums []int, target int) []int {
+	sortNums := make(twoNums, 0)
+	for idx, n := range nums {
+		t := twoNum{n, idx}
+		sortNums = append(sortNums, t)
+	}
+	sort.Sort(sortNums)
 	begin := 0
 	end := len(nums) - 1
-
 	for {
 		if begin >= end {
 			break
 		}
-		sum := nums[begin] + nums[end]
+		//sum := nums[begin] + nums[end]
+		sum := sortNums[begin].num + sortNums[end].num
 		if sum == target {
-			return []int {begin, end}
+			return []int {sortNums[begin].index, sortNums[end].index}
 		}
 		if sum > target {
 			end = end - 1
